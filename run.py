11a70e5a21f632328c0d6857e9f7293254dbca6f
antojr <anto_jr90@hotmail.com>
2016-05-26 19:54:26 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,29 +4,30 @@ import heuristic
 #game = games.TicTacToe(h=3,v=3,k=3)
 game = games.ConnectFour()
 state = game.initial
-player = 'X'
 difficultyChoosed = False
 playerChoosed = False
 
 while not difficultyChoosed:
     difficultyLevel = int(raw_input("Escoja el nivel de dificultad: 1: Facil, 2: Medio o 3: Dificil ==> "))
-    if difficultyLevel == 1 or 2 or 3:
+    if difficultyLevel == 1 or difficultyLevel == 2 or difficultyLevel == 3:
         difficultyChoosed = True
     else:
         print "Tiene que escoger 1: Facil, 2: Medio o 3: Dificil"
 
 while not playerChoosed:
-    playerStart = raw_input("Elija quien empieza primero: O: usted o X: maquina ==> ")
-    if playerStart == 'X' or 'O' or 'o' or 'x':
+    player = raw_input("Elija quien empieza primero: O: usted o X: maquina ==> ")
+    if player == 'X' or player == 'O' or player == 'o' or player == 'x':
         playerChoosed = True
     else:
         print "Tiene que escoger O: usted o X: maquina"
 
-while playerChoosed:
-    print "Jugador a mover:", game.to_move(state)
+state.to_move = player
+startPlayer=player
+while True:
+    print "Jugador a mover:", player
     game.display(state)
 
-    if player == 'O' or 'o':
+    if player == 'O' or player == 'o':
         col_str = raw_input("Movimiento: ")
         coor = int(str(col_str).strip())
         x = coor
@@ -36,18 +37,18 @@ while playerChoosed:
             if lm[0] == x:
                 y = lm[1]
     
-            state = game.make_move((x, y), state)
-            player = 'X'
+        state = game.make_move((x, y), state)
+        player = 'X'
     else:
         print "Thinking..."
         #move = games.minimax_decision(state, game)
         #move = games.alphabeta_full_search(state, game)
         if difficultyChoosed == 3:
-            move = games.alphabeta_search(state, game, 3, heuristic.calculateHeuristic, playerChoosed, 3)
+            move = games.alphabeta_search(state, game, 3, heuristic.calculateHeuristic, player, 3)
         if difficultyChoosed == 2:
-            move = games.alphabeta_search(state, game, 2, heuristic.calculateHeuristic, playerChoosed, 2)
+            move = games.alphabeta_search(state, game, 2, heuristic.calculateHeuristic, player, 2)
         else:
-            move = games.alphabeta_search(state, game, 1, None, playerChoosed, 1)
+            move = games.alphabeta_search(state, game, 1, None, player, 1)
 
         state = game.make_move(move, state)
         player = 'O'
@@ -55,7 +56,7 @@ while playerChoosed:
     if game.terminal_test(state):
         game.display(state)
         print "Final de la partida"
-        if player == playerChoosed:
+        if player != startPlayer:
             print "Has ganado"
         else:
             print "Has perdido"
